528041083dbede0bf222c7311bee321464226f82
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-08-29 06:09:04 +0530

    Day 10

---
+++ Striver/Array/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        int maxProfit = 0;
+        int maxi = prices[0];
+        int mini = prices[0];
+        for (int i = 0; i < n; i++){
+            maxi = max(maxi, prices[i]);
+            mini = min(mini, prices[i]);
+            maxProfit = max(maxProfit, prices[i] - mini);
+        }
+
+        return maxProfit;
+    }
+};
