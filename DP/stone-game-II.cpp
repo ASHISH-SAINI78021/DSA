082fcd50c5bd3fe120f00c69a30a82979ff326c2
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-09-24 18:55:33 +0530

    Day 33

--- DP/stone-game-II.cpp
+++ DP/stone-game-II.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
-    int solve(vector<int> &arr, int i, int j){
+    int solve(vector<int> &arr, int index, int m, int turn, vector<vector<vector<int>>> &dp){
         // base case
-        if (i > j) return 0;
+        if (index >= arr.size()) return 0;
+        if (dp[index][m][turn] != -1) return dp[index][m][turn];
 
-        // solution for one case 
-        int ans1 = arr[i] + max(solve(arr, i + 2, j), solve(arr, i + 1, j - 1));
-        int ans2 = arr[j] + max(solve(arr, i + 2, j - 1), solve(arr, i, j - 2));
+        // solution for one case
+        int ans;
+        if (turn == 0){
+            // alice turn
+            ans = INT_MIN;
+            int sum = 0;
+            for (int x = 0; x < 2*m && index + x < arr.size(); x++){
+                sum += arr[index + x];
+                ans = max(ans, sum + solve(arr, index + x + 1, max(x + 1, m), 1, dp));
+            }
+        }
+        else {
+            // bob turn
+            ans = INT_MAX;
+            for (int x = 0; x < 2*m && index + x < arr.size(); x++){
+                ans = min(ans, solve(arr, index + x + 1, max(x + 1, m), 0, dp));
+            }
+        }
 
-        return max(ans1, ans2);
+        return dp[index][m][turn] = ans;
     }
-    int stoneGameII(vector<int>& piles) {
-        int n = piles.size();
-        int i = 0;
-        int j = n - 1;
-
-        int ans = solve(piles, i, j);
+    int stoneGameII(vector<int>& arr) {
+        int n = arr.size();
+        int index = 0;
+        int m = 1;
+        int turn = 0;
+        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2*n + 1, vector<int>(2, -1)));
+        int ans = solve(arr, index, m, turn, dp);
 
         return ans;
     }
