c35b722dc296b9cb9d1bc4243450eb9f64945330
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-11-10 00:21:13 +0530

    Day 75

---
+++ Revision/linked-list-cycle.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL){
+            return false;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL){
+            fast = fast->next;
+            if (fast) fast = fast->next;
+            slow = slow->next;
+
+            if (slow == fast) return true;
+        }
+
+        return false
+    }
+}
