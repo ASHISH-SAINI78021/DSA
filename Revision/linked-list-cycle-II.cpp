ab906fd60d9f395499a0f8614e5142ef890a4384
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-11-11 21:38:35 +0530

    Day 2

---
+++ Revision/linked-list-cycle-II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL){
+            return NULL;
+        }
+        
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+            if (fast) fast = fast->next;
+
+            if (slow == fast){
+                slow = head;
+                while (slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                return slow;
+            }
+        }
+
+        return NULL
+    }
+}
