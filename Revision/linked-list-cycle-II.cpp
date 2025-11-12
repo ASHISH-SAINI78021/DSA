04537b14f5b35911399e76b79756161c2b0e9680
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-11-12 21:25:43 +0530

    Day 3

--- Revision/linked-list-cycle-II.cpp
+++ Revision/linked-list-cycle-II.cpp
@@ -16,8 +16,8 @@ public:
             if (slow == fast){
                 slow = head;
                 while (slow != fast){
-                    slow = slow->next;
-                    fast = fast->next;
+                    slow = slow->next
+                    fast = fast->next
                 }
 
                 return slow;
