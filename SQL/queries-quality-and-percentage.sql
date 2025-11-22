d8b63a77a1cebd52a53be2c21acef79e06fac3f0
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-11-22 11:11:12 +0530

    Day  4

---
+++ SQL/queries-quality-and-percentage.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+
+select query_name, round(sum(rating/position)/count(*), 2) as quality, round(sum(if(rating >= 3, 0, 1))/count(*)*100, 2) as poor_query_percentage
+from queries 
+group by query_name;
