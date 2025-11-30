26077502366286f6757d4dab0dcac7ad21a76dfc
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-11-30 11:34:28 +0530

    Day 1

---
+++ leetcode-150/jump-game-II.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int solve(vector<int> &nums, int index, vector<int> &dp){
+        // base case
+        if (index >= nums.size() - 1) return 0;
+        if (dp[index] != -1) return dp[index];
+
+        // solution for one case
+        int ans = INT_MAX;
+        int steps = nums[index];
+        for (int i = 1; i <= steps; i++){
+            int result = solve(nums, index + i, dp);
+            if (result != INT_MAX){
+                ans = min(ans, result + 1);
+            }
+        }
+
+        return dp[index] = ans;
+    }
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+        int index = 0;
+        vector<int> dp(n + 1, -1);
+        int ans = solve(nums, index, dp);
+
+        return ans;
+    }
+};
