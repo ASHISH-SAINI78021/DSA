e410bc161370023945d89023e6d13839fccaf639
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-04 17:28:15 +0530

    Day  1

---
+++ leetcode-150/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.length();
+        unordered_map<char, int> mp;
+        int value = 0;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        for (int i = 0; i < n; i++){
+            value += mp[s[i]];
+        }
+
+        for (int i = 1; i < n; i++){
+            if (mp[s[i - 1]] < mp[s[i]]){
+                value -= 2*mp[s[i - 1]];      
+            }
+        }
+
+        return value;
+    }
+};
