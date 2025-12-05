2fec8cbe627a748916ebb614ee2e86668d2cf34d
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-05 19:01:32 +0530

    Day  2

---
+++ leetcode-150/isomorphic-strings.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.length();
+        int m = t.length();
+        unordered_map<char, char> s_t;
+        unordered_map<char, char> t_s;
+        if (n != m) return false;
+
+        for (int i = 0; i < n; i++){
+            if (s_t.find(s[i]) == s_t.end() && t_s.find(t[i]) == t_s.end()){
+                s_t[s[i]] = t[i];
+                t_s[t[i]] = s[i];
+            }
+            else {
+                if ((s_t.find(s[i]) != s_t.end() && s_t[s[i]] == t[i]) || (t_s.find(t[i]) != t_s.end() && t_s[t[i]] == s[i])) continue ;
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
