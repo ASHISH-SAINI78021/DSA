e1d31764236b295013e0828228ff63b5a9a61f13
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-06 23:11:53 +0530

    Day 3

---
+++ leetcode-150/valid-parenthesis.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n = s.length();
+        stack<char> st;
+        for (int i = 0; i < n; i++){
+            char ch = s[i];
+            if (ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }
+            else if (ch == ')'){
+                if (st.empty() || st.top() != '(') return false;
+                st.pop();
+            }
+            else if (ch == ']'){
+                if (st.empty() || st.top() != '[') return false;
+                st.pop();
+            }
+            else if (ch == '}'){
+                if (st.empty() || st.top() != '{') return false;
+                st.pop();
+            }
+        }
+
+        if (!st.empty()) return false;
+
+        return true;
+    }
+};
