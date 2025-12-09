92f9d98ec6c7fb0e8a2314c9c1bbcfe461c755ca
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-10 03:24:35 +0530

    Day  1

--- Myntra/next_permutation.cpp
+++ Myntra/next_permutation.cpp
@@ -3,7 +3,8 @@ public:
     void nextPermutation(vector<int>& nums) {
         int n = nums.size();
         int index = -1;
-        for (int i = n - 2; i < n; i++){
+        if (n == 1) return ;
+        for (int i = n - 2; i >= 0; i--){
             if (nums[i] < nums[i + 1]){
                 index = i;
                 break;
