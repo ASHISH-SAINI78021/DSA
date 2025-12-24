7b588a1c599d34251ddec85c53301dae20759978
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-25 02:30:33 +0530

    Day  1

---
+++ NSIG/postfix-to-infix-conversion.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+
+class Solution {
+  public:   
+    bool isOperator(char &target){
+        if (target == '^' || target == '*' || target == '/' || target == '+' || target == '-') return true;
+        return false;
+    }
+    string postToInfix(string s) {
+        int n = s.length();
+        stack<string> st;
+        for (int i = 0; i < n; i++){
+            char ch = s[i];
+            if (isOperator(ch)){
+                string op1 = st.top(); st.pop();
+                string op2 = st.top(); st.pop();
+                string result = "(" + op2 + ch + op1 + ")";
+                st.push(result);
+            }
+            else st.push(string(1, ch));
+        }
+        
+        string ans = st.top();
+        return ans;
+    }
+};
