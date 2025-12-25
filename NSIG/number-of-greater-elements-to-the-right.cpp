630137eed28128381fabf79fd00f4f3805699735
ASHISH-SAINI78021 <ashu78021@gmail.com>
2025-12-25 15:42:53 +0530

    Day 2

---
+++ NSIG/number-of-greater-elements-to-the-right.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+  public:
+
+    vector<int> count_NGE(vector<int> &arr, vector<int> &indices) {
+        int n = arr.size();
+        vector<int> ans(indices.size(), 0);
+        for (int i = 0; i < indices.size(); i++){
+            int element = arr[indices[i]];
+            for (int j = indices[i]; j < n; j++){
+                if (element < arr[j]) ans[i]++;
+            }
+        }
+        
+        return ans;
+    }
+};
