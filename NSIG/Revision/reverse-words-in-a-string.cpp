d5ea99ab220fd486bfb72136cc2816b37b9898e8
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-11 15:42:36 +0530

    Day 14

---
+++ NSIG/Revision/reverse-words-in-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
+        string temp = "";
+        stack<string> st;
+
+        for (int i = 0; i < n; i++){
+            if (s[i] != ' '){
+                temp += s[i];
+            }
+            else {
+                if (!temp.empty()){
+                    st.push(temp);
+                    temp.clear();
+                }
+            }
+        }
+
+        if (!temp.empty()) st.push(temp);
+
+        while (!st.empty()){
+            auto top = st.top(); st.pop();
+            ans += top + " ";
+        }
+
+        ans.pop_back();
+
+        return ans;
+    }
+};
+
+
+// s = the sky is blue
+// o/p = blue is sky the
+
+// -> intuition -> it is like reversing the characters -> for this we've to store words and reverse the all words 
+// -> I can use stack here -> it always return the reverse order
+// -> just push word into the stack
