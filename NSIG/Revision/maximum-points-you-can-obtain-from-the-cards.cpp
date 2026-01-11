bf2dab819d7024d64f2a015f1d0d6ff33168709a
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-11 15:13:36 +0530

    Day 14

---
+++ NSIG/Revision/maximum-points-you-can-obtain-from-the-cards.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int maxi = 0;
+        int sum = 0;
+
+        for (int i = 0; i < k; i++){
+            sum += cardPoints[i];
+        }
+        maxi = max(maxi, sum);
+        for (int i = 0; i < k; i++){
+            sum = sum - cardPoints[k - i - 1] + cardPoints[n - 1 - i];
+            maxi = max(maxi, sum);
+        }
+
+        return maxi;
+    }
+};
