9e2abfaa159676eab2ca7e74eb9a0b8160e7973e
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-12 20:28:32 +0530

    Day 15

---
+++ NSIG/Revision/longest-palindromic-substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+  public:
+    int countSubstring(string s) {
+        int n = s.length();
+        int count = 0;
+        int hash[3] = {0};
+        int r = 0;
+        int l = 0;
+        while (r < n){
+            hash[s[r] - 'a']++;
+            while (hash[0] > 0 && hash[1] > 0 && hash[2] > 0){
+                count += n - r;
+                hash[s[l] - 'a']--;
+                l++;
+            }
+            r++;
+        }
+        
+        return count;
+    }
+};
+
+
+
+
+// Brute force
+// T.C -> O(n*n)
+// S.C -> O(1)                    count += n -j;
+
+
+// Optimal Approach
+// s = abcabc
+// abc-> atleast once -> then it is clear... all the substring which follows will also contain abc atleast so 
+//       count += n - r;
+//       [l....r] -> n - r
+//       [l - 1] -> iff it contains all the three elements
+//       then again check sum == 3 ? or not
