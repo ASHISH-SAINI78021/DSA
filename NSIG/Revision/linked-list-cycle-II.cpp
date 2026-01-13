b477be850bba3d29976f5db7fb27d8aefcd6212d
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-14 02:04:32 +0530

    Day 16

---
+++ NSIG/Revision/linked-list-cycle-II.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == NULL || head->next == NULL) return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL){
+            slow = slow->next;
+            fast = fast->next;
+            if (fast) fast = fast->next;
+            if (slow == fast){
+                slow = head;
+                while (slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+
+                return slow;
+            }
+        }
+
+        return NULL;
+    }
+};
