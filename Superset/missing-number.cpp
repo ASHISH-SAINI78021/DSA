c04bb37d4c62aeb362aa194740e0f24f442a63ea
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-20 21:51:28 +0530

    Day   19

---
+++ Superset/missing-number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int actualSum = accumulate(nums.begin(), nums.end(), 0);
+        int sum = n*(n + 1)/2;
+
+        return abs(actualSum - sum);
+    }
+};
