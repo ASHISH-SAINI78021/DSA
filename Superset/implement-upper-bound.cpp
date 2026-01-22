7c0412fad6ba6f753a51e7eb63b6a2d774e4accc
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-22 16:23:22 +0530

    Day 21

---
+++ Superset/implement-upper-bound.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        int n = arr.size();
+        int start = 0;
+        int end = n - 1;
+        int ans = n;
+        
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            if (arr[mid] == target){
+                ans = mid + 1;
+                start = mid + 1;
+            }
+            else if (arr[mid] > target){
+                end = mid - 1;
+            }
+            else {
+                ans = mid + 1;
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
