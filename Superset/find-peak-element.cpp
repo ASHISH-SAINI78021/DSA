20875bbb15d4890cd0be8cafdf39de5b8aeeac0f
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-23 12:07:53 +0530

    Day 22

--- Superset/find-peak-element.cpp
+++ Superset/find-peak-element.cpp
@@ -5,7 +5,7 @@ public:
         int start = 0;
         int end = n - 1;
         
-        if (n == 1) return nums[0];
+        if (n == 1) return 0s;
         if (nums[0] > nums[1]) return 0;
         if (nums[n - 1] > nums[n - 2]) return n - 1;
 
