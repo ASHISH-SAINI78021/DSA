71f60ce9136af3452ab9d68dcf910b796a12f2e4
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-23 12:11:28 +0530

    Day   22

---
+++ Superset/find-the-nth-root-of-m.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int nthRoot(int n, int m) {
+        int start = 0;
+        int end = m;
+        int ans = -1;
+        
+        while (start <= end){
+            int mid = start + (end - start)/2;
+            double target = pow(mid, n);
+            if (target <= m){
+                ans = mid;
+                start = mid + 1;
+            }
+            else end = mid - 1;
+        }
+        
+        return ans;
+    }
+};
