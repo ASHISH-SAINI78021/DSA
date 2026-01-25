40dc9f9cf2c6c81e1c71233d67a116346eb34b52
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-26 00:06:41 +0530

    Day  24

---
+++ Superset/maximum-depth-of-a-nested-parenthesis.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(string s) {
+        int n = s.length();
+        int count = 0;
+        int maxi = 0;
+
+        for (int i = 0; i < n; i++){
+            if (s[i] == '(') count++;
+            else if (s[i] == ')') count--;
+            maxi = max(maxi, count);
+        }
+
+        return maxi;
+    }
+};
