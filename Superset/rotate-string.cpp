b1aa3f89dbf7b121faaaf95905b5d8b1736374e5
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-01-25 23:45:08 +0530

    Day 24

--- Superset/rotate-string.cpp
+++ Superset/rotate-string.cpp
@@ -2,24 +2,18 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.length();
-        int m = goal.length();
-        if (n != m) return false;
-
-        for (int i = 0; i < n; i++){
-            if (s[i] == goal[0]){
-                string temp = s.substr(i) + s.substr(0, i);
-                if (temp == goal) return true;
-            }
+        int m = goal.size();
+        if (n != m){
+            return false;
         }
 
-        return false;
+        s = s + s;
+
+        return s.find(goal) != string::npos;
     }
 };
 
-
-// s = abcde   goal = cdeab
-// ab + cde == s ? -> true else return false;
-
-
-// T.C -> O(n*n)
+// s = s + s;
+// abcdeabcde goal -> will be the substring-> true : false;
+// T.C -> O(n)
 // S.C -> O(n)
