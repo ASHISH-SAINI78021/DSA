d982609dbf837ad67453a7e50322b8f9605b303b
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-05-02 20:21:20 +0530

    Day 3

--- Infosys/triangle.cpp
+++ Infosys/triangle.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &triangle, int i, int j, vector<vector<int>> &dp){
-        // base case
-        if (i == triangle.size() - 1) return triangle[i][j];
-        if (i >= triangle.size()) return 1e9;
-        if (dp[i][j] != -1) return dp[i][j];
+    // int solve(vector<vector<int>> &triangle, int i, int j, vector<vector<int>> &dp){
+    //     // base case
+    //     if (i == triangle.size() - 1) return triangle[i][j];
+    //     if (i >= triangle.size()) return 1e9;
+    //     if (dp[i][j] != -1) return dp[i][j];
 
-        // solution for one case
-        int ans1 = solve(triangle, i + 1, j, dp);
-        int ans2 = solve(triangle, i + 1, j + 1, dp);
-        return dp[i][j] = triangle[i][j] + min(ans1, ans2);
-    }
+    //     // solution for one case
+    //     int ans1 = solve(triangle, i + 1, j, dp);
+    //     int ans2 = solve(triangle, i + 1, j + 1, dp);
+    //     return dp[i][j] = triangle[i][j] + min(ans1, ans2);
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        int i = 0;
-        int j = 0;
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-        int ans = solve(triangle, i, j, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 1e9));
+        for (int j = 0; j < n; j++) dp[n - 1][j] = triangle[n - 1][j];
 
-        return ans;
+        for (int i = n - 1; i >= 0; i--){
+            for (int j = 0; j <= i; j++){
+                if (i == n - 1) continue ;
+                int ans1 = dp[i + 1][j];
+                int ans2 = dp[i + 1][j + 1];
+
+                dp[i][j] = triangle[i][j] + min(ans1, ans2);
+            }
+        }
+
+        return dp[0][0];
     }
 };
