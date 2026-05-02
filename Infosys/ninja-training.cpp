4e9a0cb92c3ceeeeabf1b054d02738d2c3bb2dec
ASHISH-SAINI78021 <ashu78021@gmail.com>
2026-05-02 18:59:28 +0530

    Day 3

--- Infosys/ninja-training.cpp
+++ Infosys/ninja-training.cpp
@@ -1,27 +1,28 @@
-int solve(vector<vector<int>> &points, int index, int prev, vector<vector<int>> &dp){
-    // base case
-    if (index >= points.size()) return 0;
-    if (dp[index][prev] != -1) return dp[index][prev];
-
-    // solution for one case
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
     int ans = 0;
-    for (int i = 0; i < 3; i++){
-        if (prev != i){
-            int result = points[index][i] + solve(points, index + 1, i, dp);
-            ans = max(ans, result);
+
+    // ? Fix DP size
+    vector<vector<int>> dp(n + 1, vector<int>(4, 0));
+
+    // ? Fill DP bottom-up
+    for (int index = n - 1; index >= 0; index--){
+        for (int j = 3; j >= 0; j--){
+            int maxi = 0;
+
+            for (int i = 0; i < 3; i++){
+                if (j != i){
+                    int result = points[index][i] + dp[index + 1][i];
+                    maxi = max(maxi, result);
+                }
+            }
+
+            dp[index][j] = maxi;
         }
     }
 
-    return dp[index][prev] = ans;
-}
-int ninjaTraining(int n, vector<vector<int>> &points)
-{
-    int index = 0;
-    int ans = 0;
-    vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
-    for (int prev = 0; prev < 3; prev++){
-        ans = max(ans, points[0][prev] + solve(points, index + 1, prev, dp));
-    }
+    // ? Final answer
+    ans = dp[0][3];
 
     return ans;
 }
